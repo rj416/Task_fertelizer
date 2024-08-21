93883d4267143c33c54d55496c0400c29c44488b
Ritesh Kumar <riteshjaishwal416@gmail.com>
2024-08-21 23:47:48 +0530

    changes1

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import { Home, Info, HelpOutline } from '@mui/icons-material';
 
 function Topbar() {
   return (
     <div className="bg-gray-800 text-white p-4 flex items-center justify-between">
       <h1 className="text-xl font-bold">Fertilizer Dashboard</h1>
       <div className="flex items-center space-x-4">
-        
+        <Home className="text-white" />
+        <Info className="text-white" />
+        <HelpOutline className="text-white" />
         <img
-          src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
+          src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
           alt="User Avatar"
           className="w-8 h-8 rounded-full"
         />
